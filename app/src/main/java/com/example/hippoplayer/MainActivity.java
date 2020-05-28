3446ae62392c5569a2f714f08c450cea048ebc3d
RickerLong <philongvadung@gmail.com>
2020-05-28 10:11:45 +0700

    Update activity main

--- app/src/main/java/com/example/hippoplayer/MainActivity.java
+++ app/src/main/java/com/example/hippoplayer/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 import com.example.hippoplayer.play.PlayFragment;
 import com.example.hippoplayer.play.ViewPagerAdapter;
@@ -19,19 +20,26 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
+        getSupportActionBar().setTitle("");
         setContentView(R.layout.activity_main);
-
+        // find view by id in activity_main
         tl_main = findViewById(R.id.tl_main);
         vp_main = findViewById(R.id.vp_main);
 
+        // create new fragment
         playFragment = new PlayFragment();
 
+        // add new fragment in function setup view pager
         setupViewPager();
         tl_main.setupWithViewPager(vp_main);
+        // seticon for fragment tabLayout.getTabAt(i).setIcon(....);
     }
 
     private void setupViewPager(){
         ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        // add new fragment
         viewPagerAdapter.addFragment(playFragment, "Player Fragment");
         vp_main.setAdapter(viewPagerAdapter);
     }
--- app/src/main/java/com/example/hippoplayer/play/PlayFragment.java
+++ app/src/main/java/com/example/hippoplayer/play/PlayFragment.java
@@ -25,7 +25,7 @@ public class PlayFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.play_fragment, container, false);
+        return inflater.inflate(R.layout.fragment_play, container, false);
     }
 
     @Override
