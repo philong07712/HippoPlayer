cebe239c0829103edbb7b2dd1b2d663d1ff8c941
RickerLong <philongvadung@gmail.com>
2020-05-29 10:32:07 +0700

    fix conflict v2

--- app/src/main/java/com/example/hippoplayer/utils/Constants.java
+++ app/src/main/java/com/example/hippoplayer/utils/Constants.java
@@ -1,12 +1,6 @@
 package com.example.hippoplayer.utils;
 
 public class Constants {
-<<<<<<< HEAD
     public static final String SONG_BASE_URL = "http://vn.dubulator.com/testHT/api";
-=======
-    public static final String SONG_URL =
-            "http://vn.dubulator.com/testHT/api/data.json?fbclid=IwAR07nN-lOIn2Ewd1rrsl2WeHG6jUQ6diZOoPQDpiE8jP7tR7a3tyc67YXmk/";
->>>>>>> parent of e72fa7e... Update ui
-
 
 }
